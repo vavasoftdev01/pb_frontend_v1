2ded4f15d9a4e156bac5df7430c017cf04ab17a0
vavasoftdev01 <vavasoftdev@outlook.com>
2024-04-22 18:08:47 +0800

    added static timer and minimal fixes on buttons

--- src/Components/Pages/home.component.tsx
+++ src/Components/Pages/home.component.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PbSingleComponent from '../Partials/betting_buttons_layouts/pb_single.component'
 
 function Home() {
@@ -38,26 +38,70 @@ function Home() {
         ]
     }
 
+    const [timer, setTimer] = useState(100) // Init 1 min..
+
+    
+
+
+    const [progressColors, setProgressColors] = useState('rgb(22 163 74)')
+
+    const timerStart = () => {
+        
+
+        setTimeout(() => {
+            setTimer((timer) => timer - 1)
+
+            
+        }, 100);
+
+        // times up
+        if(timer <= 0) {
+            timerStop()
+        }
+        
+    }
+
+    const timerStop = () => {
+       setTimer((timer) => 100)
+       console.log('timer stop')
+
+    }
+
+    useEffect(() => {
+
+        timerStart()
+    })
+
+
+    
+
   return (
     <div className="flex justify-center w-full">
         <div className="flex-col w-2/3 mt-2 rounded-lg">
-            <div className="flex flex-row gap-1 text-center font-bold text-white">
-                <div className="bg-red-600 w-full hover:cursor-pointer hover:underline p-4 hover:bg-red-700">
+            <div className="flex flex-row text-center font-bold text-white flex-nowrap">
+                <div className="bg-red-600 w-1/4 overflow-visible hover:cursor-pointer hover:underline p-4 hover:bg-red-700">
                     <a>
                         도움말
                     </a>
                 </div>
-                <div className="bg-violet-600 w-full hover:cursor-pointer hover:underline p-4 hover:bg-violet-700">
-                    <a>
+
+                <div className="w-full hover:cursor-pointer hover:underline">
+                    <img src="./images/gameplay_top.png" className="object-stretch w-full h-full"></img>
+                    <a className="absolute h-10 w-50 inset-x-30 top-8 ">
                         파워볼(1분)
                     </a>
                 </div>
-                <div className="bg-blue-600 w-full hover:cursor-pointer hover:underline p-4 hover:bg-blue-700">
+
+                <div className="bg-blue-600 w-1/4 hover:cursor-pointer hover:underline p-4 hover:bg-blue-700">
                     설정
                 </div>
+
             </div>
-            <div className="bg-amber-500">
-                <h1>Timer - TODO </h1>
+            <div className="w-full flex flex-col bg-slate-700">
+                <div className="bg-slate-800 relative mb-6">
+                    <span className="flex absolute" style={{ width: timer+'%', transition: '0.1s', transitionTimingFunction: 'linear', backgroundColor: progressColors }}>&nbsp;</span>
+                    <span className="absolute bottom-0 left-0 right-0 top-0 grid place-items-center text-white">배팅하세요 - 00:{timer}</span>
+                </div>
             </div>
             <div className="flex flex-row bg-gray-100">
                 <div className="w-full flex flex-col gap-3 p-2">
