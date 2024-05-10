735b59992c82cad9d3843df8359daa4ab7dbfe2a
vavasoftdev01 <vavasoftdev@outlook.com>
2024-05-10 14:14:53 +0800

    filter absolute

--- src/Components/Partials/pb_stats_panel.component.tsx
+++ src/Components/Partials/pb_stats_panel.component.tsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function PBStatsPanelComponent(props) {
+    const [filter1, setFilter1] = useState(false)
+    const [pbFilter, setPbFilter] = useState(true) // pb_form & daily_analysis_form
+
+    const [filter2, setFilter2] = useState(false)
+    
+
+    const toggleFilter1 = () => {
+        setFilter1(!filter1)
+    }
+
+    const handleFilter1FormToggle = () => {
+        setPbFilter(!pbFilter)
+        console.log(pbFilter)
+        
+    }
+
+    const toggleFilter2 = () => {
+        setFilter2(!filter2)
+    }
 
     return(
         <>
@@ -19,21 +38,47 @@ function PBStatsPanelComponent(props) {
                         </button>
                     </div>
                 </div>
-                {/* Content goes here... */}
-                <div className="inline-flex">
-                    <select className="w-full border-2 border-solid border-amber-600 rounded-md p-2 mt-2 mx-1">
-                        <option className="p-2 border-amber-600 ">파워볼</option>
-                        <option className="p-2 border-amber-600 ">일별분석</option>
-                    </select>
-                    <select className="w-full border-2 border-solid border-amber-600 rounded-md p-2 mt-2 mx-1">
-                        <option className="p-2 ">파워볼 홀/짝</option>
-                        <option className="p-2 ">파워볼 언더/오버</option>
-                        <option className="p-2 ">일반볼 홀/짝</option>
-                        <option className="p-2 ">일반볼 언더/오버</option>
-                    </select>
+                {/* Content goes here... Filters*/}
+                <div className="relative">
+                    <div className="filter-1 w-[48%] border-2 border-solid border-amber-600 rounded-md px-2 py-1 mt-2 mx-1 flex flex-col bg-[#fce6d2] absolute ml-2" onClick={toggleFilter1}>
+                        <div className="relative filter1-cont">
+                            {/* Powerball filter */}
+                            <div className="r1 flex flex-row justify-between w-full p-1 cursor-pointer hover:text-white">
+                                <span className="" onClick={() => handleFilter1FormToggle}>파워볼</span>
+                                <button className={(filter1 ? " rotate-180":" rotate-0")}>
+                                    <svg width="15" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M20.54 14.6404C20.54 15.0626 20.3627 15.4848 20.0082 15.7861C19.3988 16.3039 18.4683 16.1897 17.9029 15.6243L12.0361 9.75807L6.09179 15.7024C5.50585 16.2884 4.55663 16.2884 3.9707 15.7024C3.38477 15.1165 3.38476 14.1673 3.9707 13.5813L11.329 6.22213C11.7195 5.83156 12.3527 5.83155 12.7433 6.22211L20.1006 13.5799C20.3936 13.8728 20.54 14.2566 20.54 14.6404Z" fill="#000"></path></svg>
+                                </button>
+                            </div>
+                            {/* Daily Analysis filter */}
+                            <div className={"r1 flex flex-row justify-between w-full border-solid border-t-2 border-slate-300 cursor-pointer hover:text-white "+(filter1 ? "hidden":"p-1")} onClick={() => handleFilter1FormToggle()}>
+                                <span className="">일별분석</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="filter-1 w-[49%] border-2 border-solid border-[#939ed2] rounded-md px-2 py-1 mt-2 mx-1 flex flex-col bg-[#eeeeee] absolute ml-[33.3rem]" onClick={toggleFilter2}>
+                        <div className="relative filter2-cont">
+                            <div className="r1 flex flex-row justify-between w-full p-1 cursor-pointer hover:text-white">
+                                <span className="">파워볼 홀/짝</span>
+                                <button className={(filter2 ? " rotate-180":" rotate-0")}>
+                                    <svg width="15" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M20.54 14.6404C20.54 15.0626 20.3627 15.4848 20.0082 15.7861C19.3988 16.3039 18.4683 16.1897 17.9029 15.6243L12.0361 9.75807L6.09179 15.7024C5.50585 16.2884 4.55663 16.2884 3.9707 15.7024C3.38477 15.1165 3.38476 14.1673 3.9707 13.5813L11.329 6.22213C11.7195 5.83156 12.3527 5.83155 12.7433 6.22211L20.1006 13.5799C20.3936 13.8728 20.54 14.2566 20.54 14.6404Z" fill="#000"></path></svg>
+                                </button>
+                            </div>
+
+                            <div className={"r1 flex flex-row justify-between w-full border-solid border-t-2 border-slate-300 cursor-pointer hover:text-white "+(filter2 ? "hidden":"p-2")}>
+                                <span className="">파워볼 언더/오버</span>
+                            </div>
+                            <div className={"r1 flex flex-row justify-between w-full border-solid border-t-2 border-slate-300 cursor-pointer hover:text-white "+(filter2 ? "hidden":"p-2")}>
+                                <span className="">일반볼 홀/짝</span>
+                            </div>
+                            <div className={"r1 flex flex-row justify-between w-full border-solid border-t-2 border-slate-300 cursor-pointer hover:text-white "+(filter2 ? "hidden":"p-2")}>
+                                <span className="">일반볼 언더/오버</span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
-                <div className="avg-stats-24hrs border-[0.1rem] border-solid border-gray-400 mt-2 rounded-md p-2 mx-2 flex flex-col">
+                <div className="avg-stats-24hrs border-[0.1rem] border-solid border-gray-400 mt-[4rem] rounded-md p-2 mx-2 flex flex-col">
                     <div className="flex flex-row">
                         <div className="flex flex-col w-full">
                             <div className="w-full inline-flex">
